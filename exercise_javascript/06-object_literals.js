61cb725a33aa9bab95a93f43f082e626fe863437
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-10 17:09:06 -0300

    chore: add missing questions

--- exercise_javascript/06-object_literals.js
+++ exercise_javascript/06-object_literals.js
@@ -3,6 +3,8 @@
 // 3. Acesse uma propriedade de um literal de objeto usando notação de colchetes e registre-a.
 // 4. Adicione uma nova propriedade a um literal de objeto existente e registre o objeto atualizado.
 
+const { ifError } = require("assert");
+
 const person = {
     name: 'Maria',
     age: 25,
@@ -31,23 +33,91 @@ client.msg()
 
 // 6. Use nomes de propriedades computadas para criar um literal de objeto e registrá-lo.
 
-const registro = {
+const test6 = {
   [`data_${new Date().toLocaleDateString()}`]: {
-    hora: new Date().toLocaleTimeString(),
-    mensagem: 'Registro criado com sucesso!'
+    hour: new Date().toLocaleTimeString(),
+    message: 'Registro criado com sucesso!'
   }
 };
 
-console.log(registro);
+console.log(test6);
 
 // 7. Crie um literal de objeto com uma propriedade que seja um array. Registre o objeto.
 
-const pedido = {
-  
+const delivery = {
+  client: 'Felipe',
+  order: '01B3',
+  price: 31.50,
+  address: ['Rua Sobral, 23.', 'Rua Caetano, 40.', 'Rua Sossego, 900.']
 }
+console.log(delivery.address)
+console.log(delivery.address[0]);
+console.log(delivery['address'][1]);
+
 
 // 8. Crie um literal de objeto com um objeto aninhado e registre-o.
 
+const test8 = {
+  nome: 'Silva',
+  idade: 23,
+  endereco: {
+    rua: 'Rua dos Pinheiros',
+    numero: 123,
+    bairro: 'Centro',
+    cidade: 'Fortaleza',
+    estado: 'CE'
+  }
+};
+const test8City = test8.endereco.cidade
+console.log(test8)
+console.log(test8City)
+
 // 9. Escreva uma função que receba um literal de objeto e retorne um novo objeto com as chaves e valores invertidos.
+/*
+const teste = [
+  ['a', 1],
+  ['b', 2],
+  ['c', 3]
+]
+console.log(Object.fromEntries(teste)) // { a: 1, b: 2, c: 3 }
+console.log(Object.entries(teste))//[ [ '0', [ 'a', 1 ] ], [ '1', [ 'b', 2 ] ], [ '2', [ 'c', 3 ] ] ]
+*/
+
+const order = {
+  blusa: 32,
+  sapato: 22,
+  camiseta: 10,
+}
+
+function inverterObjeto(obj) {
+  return Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]));
+}
+
+console.log(inverterObjeto(order))
 
 // 10. Crie um literal de objeto com propriedades getter e setter e demonstre seu uso.
+
+const matricula = {
+  aluno: '',
+  disciplina: 'Python e IA',
+  sala: 201,
+
+  get insertAluno(){
+    return this.aluno
+  },
+  set insertAluno(value){
+    if(value === ''){
+      throw new Error('O campo "aluno" não pode ser vazio.')
+    }
+    this.aluno = value
+  }
+}
+
+matricula.insertAluno = 'Paulo'
+console.log(matricula.insertAluno)
+
+// try {
+//   matricula.insertAluno = '';
+// } catch (e) {
+//   console.error(e);
+// }
