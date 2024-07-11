4d72019a0d6ea8f1be69d11235fa5c83315dd586
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-11 09:50:32 -0300

    chore: conclusion of questions

--- exercise_typescript/01-tipos_basicos.ts
+++ exercise_typescript/01-tipos_basicos.ts
@@ -1,17 +1,37 @@
 // 1. Declare variáveis em TypeScript usando os tipos básicos: string, number e boolean.
 
-// const nome: string = "thalia"
-// const idade: number = 27
-// const adulto: boolean = true
+const nome: string = "thalia"
+const idade: number = 27
+const adulto: boolean = true
 
 // 2. Escreva uma função que receba dois números e retorne a soma deles. Tipifique corretamente os parâmetros e o retorno da função.
 
-// function calc(x: number, y: number): number {
-//   return x + y;
-// }
-// const resultado = calc(10, 20)
+function calc(x: number, y: number): number {
+  return x + y;
+}
+const result = calc(30, 20)
 
-// console.log(resultado)
+console.log(result)
 
 // 3. Crie uma interface que descreva um objeto com propriedades de diferentes tipos básicos (string, number, boolean). Em seguida, escreva uma função que receba esse objeto e imprima suas propriedades no console.
 
+interface MyObject {
+  name: string;
+  age: number;
+  Admin: boolean;
+  [key: string]: string | number | boolean;
+}
+
+function ObjectProp(obj: MyObject) {
+  for (const prop in obj) {
+    console.log(`${prop}: ${obj[prop]}`);
+  }
+}
+
+const myObject: MyObject = {
+  name: 'Adriano',
+  age: 34,
+  Admin: true
+};
+
+ObjectProp(myObject);
