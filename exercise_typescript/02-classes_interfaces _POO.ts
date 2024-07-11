d8d574e97e3d45d8d6f3bf218852d05089799630
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-11 10:27:52 -0300

    build: question file.

---
+++ exercise_typescript/02-classes_interfaces _POO.ts
@@ -0,0 +1,4 @@
+// 1. Crie uma classe `Pessoa` com propriedades `nome` e `idade`, e um método que retorne uma saudação incluindo essas informações.
+// 2. Implemente uma interface `Animal` com métodos `emitirSom` e `mover`. Crie uma classe `Cachorro` que implemente essa interface.
+// 3. Crie uma hierarquia de classes para representar diferentes tipos de veículos. A classe base `Veiculo` deve ter propriedades e métodos comuns,
+// enquanto as classes derivadas `Carro` e `Moto` devem estender `Veiculo` e adicionar funcionalidades específicas.
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,127 @@
-//Tipos básicos
-let nome: string = 'Thalia'
-let idade: number = 27
-let adulto: boolean = true
-let simbolo: symbol = Symbol('qualquer-symbol')
+//Tipos Básicos*
+// let nome: string = 'Thalia'
+// let idade: number = 27
+// let adulto: boolean = true
+// let simbolo: symbol = Symbol('qualquer-symbol')
 
-// Array
-let arrayNumber: Array<number> = [1, 2, 3]
-let arrayNumber2: number[] = [1, 2, 3]
-let arrayStrings: Array<string> = ['a', 'b', 'c']
-let arrayStrings2: string[] = ['a', 'b', 'c']
+import { PassThrough } from "stream";
 
-// Objetos
-let pessoa: {
-  nome: string,
-  idade: number,
-  adulto?: boolean
-} = {
-  nome: 'pedro',
-  idade: 35,
-}
+// Array*
+// let arrayNumber: Array<number> = [1, 2, 3]
+// let arrayNumber2: number[] = [1, 2, 3]
+// let arrayStrings: Array<string> = ['a', 'b', 'c']
+// let arrayStrings2: string[] = ['a', 'b', 'c']
 
-// Funções
-function soma(x: number, y: number): number {
-  return x + y
-}
-const soma2: (x: number, y: number) => number = (x, y) => x + y
+// Objetos*
+// let pessoa: {
+//   nome: string,
+//   idade: number,
+//   adulto?: boolean
+// } = {
+//   nome: 'pedro',
+//   idade: 35,
+// }
+
+// Funções*
+// function soma(x: number, y: number): number {
+//   return x + y
+// }
+// const soma2: (x: number, y: number) => number = (x, y) => x + y
 
 
-// Objetos part2
-const objetoA: {
-  readonly chaveA: string;  //não pode ser alterada
-  chaveB: string;
-  chaveC?: string;
-  [key: string]: unknown;
-} = {
-  chaveA:'Outro valor',
-  chaveB: 'Valor de B',
-}
+// Objetos part2*
+// const objetoA: {
+//   readonly chaveA: string;  //não pode ser alterada
+//   chaveB: string;
+//   chaveC?: string;
+//   [key: string]: unknown;
+// } = {
+//   chaveA:'Outro valor',
+//   chaveB: 'Valor de B',
+// }
 
-objetoA.chaveB = 'novo valor B'
-objetoA.chaveC = 'novo valor C'
-objetoA.chaveD = 123
+// objetoA.chaveB = 'novo valor B'
+// objetoA.chaveC = 'novo valor C'
+// objetoA.chaveD = 123
 
-console.log(objetoA)
+// console.log(objetoA)
 
-// array tuple
-const dadosCliente: [number, string] = [1, 'Luiz']
+// Array Tuple*
+// const dadosCliente: [number, string] = [1, 'Luiz']
 
-dadosCliente[0] = 10;
-dadosCliente[1] = 'Jose'
-console.log(dadosCliente)
+// dadosCliente[0] = 10;
+// dadosCliente[1] = 'Jose'
+// console.log(dadosCliente)
 
 // Array Readonly
-const array1: readonly string[] = ['Ana', 'Paulo']
-const array2: ReadonlyArray<string> = ['Maria', 'Joao']
+// const array1: readonly string[] = ['Ana', 'Paulo']
+// const array2: ReadonlyArray<string> = ['Maria', 'Joao']
 
-console.log(array1)
-console.log(array2)
+// console.log(array1)
+// console.log(array2)
 
+// Type Alias*
+// type Idade = number;
+// type Pessoa = {
+//   nome: string;
+//   idade: Idade;
+//   salario: number;
+//   corFavorita?: string;
+// }
+// type CorRGB = 'Vermelho' | "Verde" | "Azul";
+// type CorCMYL = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+// type corFavorita = CorRGB | CorCMYL;
 
+// const pessoa: Pessoa = {
+//   nome: 'Paula',
+//   idade: 29,
+//   salario: 2.000
+// }
+
+// export function setCorFavorita(pessoa: Pessoa, cor: corFavorita): Pessoa {
+//   return {...pessoa, corFavorita: cor}
+// }
+
+// console.log(setCorFavorita(pessoa, 'Vermelho'))
+
+// Types Intersection*
+//type Intersecao = AB & AC & AD;
+// type Temnome = { nome: string };
+// type TemSobrenome = { sobrenome: string };
+// type TemIdade = { idade: number };
+// type Person = Temnome & TemSobrenome &  TemIdade;
+
+// const pessoa: Person = {
+//   nome: 'Lucas',
+//   sobrenome: 'Silva',
+//   idade: 32,
+// }
+// console.log(pessoa)
+
+// Structual Typing*
+// type VerifyUserFn = (user: User, sentValue: User) => boolean
+// type User = { username: string, password: string}
+
+// const verifyUser: VerifyUserFn = (user, sentValue) => {
+//   return (
+//     user.username === sentValue.username && user.password === sentValue.password
+//   );
+// }
+
+// const bdUser = {username: 'thiago', password: '1234'}
+// const sentUser = {username: 'thiago', password: '1234'}
+// const loggedIn = verifyUser(bdUser, sentUser)
+
+// console.log(loggedIn)
+
+// This em Funções*
+// function funcao(this: Date, argumento1: string): void {
+//   console.log(this)
+//   console.log(argumento1)
+// }
+
+// funcao.call(new Date(), 'Hoje')
+// funcao.apply(new Date(), ['Hoje'])
 
 
 
