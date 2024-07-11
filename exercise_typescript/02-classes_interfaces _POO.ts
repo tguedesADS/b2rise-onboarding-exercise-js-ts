bc5830486098753069a0a03573b28dbd83e897a5
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-11 15:37:55 -0300

    chore: add resolution for question 02

--- exercise_typescript/02-classes_interfaces _POO.ts
+++ exercise_typescript/02-classes_interfaces _POO.ts
@@ -17,7 +17,7 @@
 // console.log(pessoa1.saudacao())
 
 export class Pessoa {
-  constructor(public nome: string, public idade: number) {}
+  constructor(public nome: string, public idade: number) { }
 
   public saudacao(): string {
     return `Olá, eu sou ${this.nome} e tenho ${this.idade} anos.`
@@ -28,7 +28,26 @@ console.log(pessoa1.saudacao())
 
 // 2. Implemente uma interface `Animal` com métodos `emitirSom` e `mover`. Crie uma classe `Cachorro` que implemente essa interface.
 
+interface Animal {
+  emitirSom(): string;
+  mover(): string;
+}
 
+export class Cachorro implements Animal {
+  constructor(private nome: string) {}
+
+  emitirSom(): string {
+    return `O ${this.nome} está latindo`;
+  }
+
+  mover(): string {
+    return `O ${this.nome} está andando`;
+  }
+}
+
+const meuCachorro = new Cachorro('Bob');
+console.log(meuCachorro.emitirSom());
+console.log(meuCachorro.mover());
 
 // 3. Crie uma hierarquia de classes para representar diferentes tipos de veículos. A classe base `Veiculo` deve ter propriedades e métodos comuns,
 // enquanto as classes derivadas `Carro` e `Moto` devem estender `Veiculo` e adicionar funcionalidades específicas.
