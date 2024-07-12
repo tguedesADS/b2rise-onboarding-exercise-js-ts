c709daf051bd91a5c63f1e39658beafeb31f9d7a
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-12 10:16:51 -0300

    refactor: change in some resolutions

--- exercise_typescript/02-classes_interfaces _POO.ts
+++ exercise_typescript/02-classes_interfaces _POO.ts
@@ -16,16 +16,6 @@ export class Pessoa {
 const pessoa1 = new Pessoa('thalia', 27)
 console.log(pessoa1.saudacao())
 
-export class Pessoa {
-  constructor(public nome: string, public idade: number) { }
-
-  public saudacao(): string {
-    return `Olá, eu sou ${this.nome} e tenho ${this.idade} anos.`
-  }
-}
-const pessoa1 = new Pessoa('thalia', 27)
-console.log(pessoa1.saudacao())
-
 // 2. Implemente uma interface `Animal` com métodos `emitirSom` e `mover`. Crie uma classe `Cachorro` que implemente essa interface.
 
 interface Animal {
