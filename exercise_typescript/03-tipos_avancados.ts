7a5336cbcd5366fd6ccb84b844e88e9bec44b714
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-12 10:43:36 -0300

    build: question file.

---
+++ exercise_typescript/03-tipos_avancados.ts
@@ -0,0 +1,5 @@
+// 1. Utilize o tipo `union` para criar uma variável que pode ser uma string ou um número.
+// 2. Defina um tipo `alias` para um objeto que represente um ponto no espaço 2D, com propriedades `x` e `y`.
+// Escreva uma função que calcule a distância entre dois pontos.
+// 3. Crie um tipo `mapped` que transforme todas as propriedades de um objeto em opcionais.
+// Utilize esse tipo para definir uma função que atualize parcialmente um objeto, aplicando somente as mudanças fornecidas.
