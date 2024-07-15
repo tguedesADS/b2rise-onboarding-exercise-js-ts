726fc873eaa0a970eaed6ff652d4e8aa42399cc2
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-15 13:11:07 -0300

    build: question file.

---
+++ exercise_typescript/04-generics.ts
@@ -0,0 +1,3 @@
+// 1. Crie uma função genérica que receba um array de elementos e retorne o primeiro elemento.
+//   2. Implemente uma classe genérica `Stack` com métodos para adicionar um elemento ao topo, remover o elemento do topo e verificar se a pilha está vazia.
+//   3. Crie uma função genérica que receba um objeto e um array de chaves. A função deve retornar um novo objeto contendo apenas as propriedades especificadas pelas chaves.
