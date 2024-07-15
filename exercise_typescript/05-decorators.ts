297fc0bf7a6a366614cdc955e7f0af75bd1e5b1e
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-15 17:21:36 -0300

    build: question file.

---
+++ exercise_typescript/05-decorators.ts
@@ -0,0 +1,14 @@
+// 1. Crie um decorator de método que registre o tempo de execução de uma função.
+
+
+
+// 2. Implemente um decorator de propriedade que valide se um valor atribuído a uma
+// propriedade string não é vazio.
+
+
+
+// 3. Crie um decorator de classe que adicione um método estático à classe para criar
+// instâncias usando um objeto de configuração.
+
+
+
