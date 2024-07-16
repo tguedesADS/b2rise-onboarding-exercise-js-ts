10977add25cddd6d09b09ed857476bae9921bf11
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-16 14:05:53 -0300

    chore: resolution of all questions

--- exercise_typescript/06-namespaces_e_modulos.ts
+++ exercise_typescript/06-namespaces_e_modulos.ts
@@ -1,6 +1,13 @@
 // 1. Crie um namespace `Util` que contenha uma função para calcular a área de um círculo.
+namespace Util {
+  export function calculateCircleArea(radius: number): number {
+    return Math.PI * (radius ** 2);
+  }
+}
 
-
+console.log(`A área do círculo é ${Util.calculateCircleArea(4).toFixed(2)}`);
 
 // 2. Separe um projeto em múltiplos módulos, onde cada módulo exporta uma função ou classe
 // diferente. Utilize `import` e `export` para conectar os módulos.
+
+//Path: exercise_typescript/project_calc
