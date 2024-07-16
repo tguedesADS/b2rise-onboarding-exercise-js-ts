f473663e37d6d7bf45a96bbacc9965d916f2988f
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-16 16:59:22 -0300

    build: question file.

---
+++ exercise_typescript/07-principios_solid.ts
@@ -0,0 +1,10 @@
+// 1. Implemente o princípio de responsabilidade única criando uma classe `Usuario` que seja responsável apenas pela manipulação de dados de usuário.
+
+
+
+// 2. Aplique o princípio da substituição de Liskov implementando uma classe base `Forma` e classes derivadas `Quadrado` e `Retangulo`que mantenham a consistência do comportamento
+
+
+
+// 3. Utilize os princípios de segregação de interfaces e inversão de dependência para criar um sistema de pagamento modular,
+// onde diferentes métodos de pagamento (cartão, boleto, etc.) podem ser facilmente integrados e substituídos.
