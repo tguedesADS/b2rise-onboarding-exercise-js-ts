12306d65c7d7b32abc169428e7e455907570d16f
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-16 13:16:36 -0300

    build: question file.

---
+++ exercise_typescript/06-namespaces_e_modulos.ts
@@ -0,0 +1,6 @@
+// 1. Crie um namespace `Util` que contenha uma função para calcular a área de um círculo.
+
+
+
+// 2. Separe um projeto em múltiplos módulos, onde cada módulo exporta uma função ou classe
+// diferente. Utilize `import` e `export` para conectar os módulos.
